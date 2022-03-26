49e59ccb4323d16f375546b7a605c91e780a018a
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-03-26 17:01:29 +0600

    data and component is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Cart from './components/cart/Cart';
+import Header from './components/header/Header';
+import Product from './components/product/Product';
 
 function App() {
   return (
     <div className="App">
-      
+        <Header/>
+        <div className="main">
+          <div className="products">
+            <Product/>  
+          </div>
+          <Cart/>
+        </div>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+        Food <span>Picker</span> 
+    </div>
+  )
+}
+
+export default Header
